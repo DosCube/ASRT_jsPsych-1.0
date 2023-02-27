67fff6e80f66cec52884b47872a1f29aee629ada
Scott Dawson <scott@automated-intelligence.com>
2023-02-27 03:23:56 +0000

    Add project files.

---
+++ shared/parameters.js
@@ -0,0 +1,11 @@
+const language = en; //available languages: english (en), hungarian (hu), french (fr), portoguise (pt)
+const numberOfPracticeBlocks = 0;
+const numberOfBlocks = 20;
+const numberOfBlockElements = 85;
+const numberOfSequenceRepetitions = 10;
+const patternTrialImage = "url(../static/images/dalmata.jpg)";
+const randomTrialImage = "url(../static/images/dalmata.jpg)";
+const secondTrialImage = "url(../static/images/penguin.jpg)";
+const blockToBeginUsingSecondImage = 3;
+const rsi = 120;
+const initialDelay = 1000;
